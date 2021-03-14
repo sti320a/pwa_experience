cc4bf7f3a8bf122530baa2507b3636cedfde7e3d
sti320a <sti320a@gmail.com>
2021-03-14 14:51:08 +0900

    Add initial files

---
+++ js/service_worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'pwa-caches';
+const USER_TO_CACHE = [
+    '/example.com/',
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function (response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
